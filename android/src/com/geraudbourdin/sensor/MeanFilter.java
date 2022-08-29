fa66321e8a93da5f95ec78a302d397e957d2a574
m1ga <migamiga@web.de>
2022-08-29 19:43:58 +0200

    update

--- android/src/com/geraudbourdin/sensor/MeanFilter.java
+++ android/src/com/geraudbourdin/sensor/MeanFilter.java
@@ -7,89 +7,76 @@ import java.util.LinkedList;
 import java.util.List;
 
 
-public class MeanFilter
-{
-	// The size of the mean filters rolling window.
-	private int filterWindow = 30;
+public class MeanFilter {
+    // The size of the mean filters rolling window.
+    private int filterWindow = 30;
 
-	private boolean dataInit;
+    private boolean dataInit;
 
-	private ArrayList<LinkedList<Number>> dataLists;
+    private final ArrayList<LinkedList<Number>> dataLists;
 
-	/**
-	 * Initialize a new MeanFilter object.
-	 */
-	public MeanFilter()
-	{
-		dataLists = new ArrayList<LinkedList<Number>>();
-		dataInit = false;
-	}
+    /**
+     * Initialize a new MeanFilter object.
+     */
+    public MeanFilter() {
+        dataLists = new ArrayList<LinkedList<Number>>();
+        dataInit = false;
+    }
 
-	/**
-	 * Filter the data.
-	 * 
-	 * @param iterator
-	 *            contains input the data.
-	 * @return the filtered output data.
-	 */
-	public float[] filterFloat(float[] data)
-	{
-		for (int i = 0; i < data.length; i++)
-		{
-			// Initialize the data structures for the data set.
-			if (!dataInit)
-			{
-				dataLists.add(new LinkedList<Number>());
-			}
+    /**
+     * Filter the data.
+     *
+     * @param iterator contains input the data.
+     * @return the filtered output data.
+     */
+    public float[] filterFloat(float[] data) {
+        for (int i = 0; i < data.length; i++) {
+            // Initialize the data structures for the data set.
+            if (!dataInit) {
+                dataLists.add(new LinkedList<Number>());
+            }
 
-			dataLists.get(i).addLast(data[i]);
+            dataLists.get(i).addLast(data[i]);
 
-			if (dataLists.get(i).size() > filterWindow)
-			{
-				dataLists.get(i).removeFirst();
-			}
-		}
+            if (dataLists.get(i).size() > filterWindow) {
+                dataLists.get(i).removeFirst();
+            }
+        }
 
-		dataInit = true;
+        dataInit = true;
 
-		float[] means = new float[dataLists.size()];
+        float[] means = new float[dataLists.size()];
 
-		for (int i = 0; i < dataLists.size(); i++)
-		{
-			means[i] = (float) getMean(dataLists.get(i));
-		}
+        for (int i = 0; i < dataLists.size(); i++) {
+            means[i] = getMean(dataLists.get(i));
+        }
 
-		return means;
-	}
+        return means;
+    }
 
-	/**
-	 * Get the mean of the data set.
-	 * 
-	 * @param data
-	 *            the data set.
-	 * @return the mean of the data set.
-	 */
-	private float getMean(List<Number> data)
-	{
-		float m = 0;
-		float count = 0;
+    /**
+     * Get the mean of the data set.
+     *
+     * @param data the data set.
+     * @return the mean of the data set.
+     */
+    private float getMean(List<Number> data) {
+        float m = 0;
+        float count = 0;
 
-		for (int i = 0; i < data.size(); i++)
-		{
-			m += data.get(i).floatValue();
-			count++;
-		}
+        for (int i = 0; i < data.size(); i++) {
+            m += data.get(i).floatValue();
+            count++;
+        }
 
-		if (count != 0)
-		{
-			m = m / count;
-		}
+        if (count != 0) {
+            m = m / count;
+        }
 
-		return m;
-	}
+        return m;
+    }
 
-	public void setWindowSize(int size)
-	{
-		this.filterWindow = size;
-	}
+    public void setWindowSize(int size) {
+        this.filterWindow = size;
+    }
 }
